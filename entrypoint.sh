6e96d554c5ce083a0c17e890f82a468a5044ae4c
Mitch Talmadge <mitch@mitchtalmadge.com>
2020-06-07 22:22:30 -0600

    Check if config files exist, and generate if missing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 set +o xtrace
 
+# Create user and group that will own the config files.
 if [ ! "$(getent group appgroup)" ]
 then
   # Create group
@@ -17,8 +18,16 @@ then
     --system \
 		appuser
 fi
-
 chown -R appuser:appgroup /app/mount
 
-su appuser -c "shreddit -g" && \
+# Check for existence of config files and generate if missing
+PRAW_FILE=/app/mount/praw.ini
+CONFIG_FILE=/app/mount/shreddit.yml
+if [[ ! -f "$PRAW_FILE" || ! -f "$CONFIG_FILE" ]]; then
+	echo "One or more config files are missing. Generating config files..."
+	su appuser -c "shreddit -g" && \
+	echo "Config files generated. Please fill them in and then restart the container."
+	exit 0
+fi
+
 su appuser -c "shreddit"
